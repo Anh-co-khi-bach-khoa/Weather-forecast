239b2e253a79ca5549bd6f0a915c16ec6ef5a410
Nguyen Tuan <tuanmeme666@gmail.com>
2023-11-13 17:47:34 +0700

    Hoàn thành animation khi mouseover và mouseout từng tỉnh, click vào tỉnh có thể lấy ra id của tỉnh đó

---
+++ html/app.js
@@ -0,0 +1,25 @@
+gsap.set (".province", {scale:1});
+
+document.querySelectorAll(".province").forEach((province)=>{
+  province.addEventListener("mouseover", ()=>{
+    gsap.to(province, {scale: 1.5, duration:0.5});
+    province.style.fillOpacity = "1";
+    // province.style.zIndex = "1";
+    
+    // province.setAttribute("opacity", "1");
+  })
+  province.addEventListener("mouseout", ()=>{
+    gsap.to(province, {scale:1, duration:0.5});
+    province.style.fillOpacity = "0.56";
+    // province.style.zIndex = "0";
+    // province.setAttribute("opacity", "0.56");
+  })
+});
+
+document.querySelectorAll(".province").forEach((province) => {
+    province.addEventListener("click", () => {
+      const provinceId = province.getAttribute("id");
+      alert("ID:" + provinceId);
+    });
+  });
+
